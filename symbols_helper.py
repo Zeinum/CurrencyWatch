f1a21d7825e0d0be3a7b2b237370de48caa8eecc
Zeinum <Zeinum@gmail.com>
2021-05-13 23:03:26 +0300

    added db with symbols

--- symbols_helper.py
+++ symbols_helper.py
@@ -1,6 +1,27 @@
 import requests
-import json
+import dataset
 
-r = requests.get("https://api-adapter.backend.currency.com/api/v1/exchangeInfo")
-for i in r.json()['symbols']:
-    print(i['symbol']," - ", i['name']," - ", i['sector']," - ", i['industry'])
+db = dataset.connect('sqlite:///symbol_info.db')
+table = db['symbols']
+
+#manual use, erase shortlisted values
+def update_symbols():
+    r = requests.get("https://api-adapter.backend.currency.com/api/v1/exchangeInfo")
+    for i in r.json()['symbols']:
+        table.insert(dict(symbol=i['symbol'], name=i['name'], sector=i['sector'], industry=i['industry'], shortlisted = 0 ))
+
+def get_shortlisted_symbols():
+    shortlisted = table.find(shortlisted=1)
+    print([i['symbol'] for i in shortlisted])
+
+def get_all_unlisted_symbols():
+    symbols = table.find(shortlisted = 0)
+    unlisted = [i['symbol'] for i in symbols]
+    print(unlisted)
+    print(len(unlisted))
+
+def shortlist_symbol(symbol):
+    table.update(dict(symbol=symbol, shortlisted=1), ['symbol'])
+
+def unlist_symbol(symbol):
+    table.update(dict(symbol=symbol, shortlisted=0), ['symbol'])
